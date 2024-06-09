21318c8b860d5897589cc0e7e61d6a317b7f86a2
AIDA64S <zxc17634985169@gmail.com>
2024-06-10 00:02:43 +0800

    test fix iris (but need delete 1.16.5)

--- common/src/main/java/org/teacon/slides/renderer/SlideRenderType.java
+++ common/src/main/java/org/teacon/slides/renderer/SlideRenderType.java
@@ -1,53 +1,65 @@
 package org.teacon.slides.renderer;
 
-import com.google.common.collect.ImmutableList;
+import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.DefaultVertexFormat;
-import net.minecraft.client.renderer.RenderStateShard;
+import com.mojang.blaze3d.vertex.VertexFormat;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.resources.ResourceLocation;
 import org.teacon.slides.Slideshow;
-import org.teacon.slides.mappings.UtilitiesClient;
-
-import java.util.Objects;
 
 /**
  * @author BloCamLimb
  */
-public class SlideRenderType extends RenderType {
+public class SlideRenderType extends RenderType.CompositeRenderType {
+    public SlideRenderType(int texture) {
+        super(Slideshow.ID, DefaultVertexFormat.BLOCK,
+                VertexFormat.Mode.QUADS, 256, false, true,
+                CompositeState.builder()
+                        .setShaderState(RENDERTYPE_TEXT_SEE_THROUGH_SHADER)
+                        .setTransparencyState(TRANSLUCENT_TRANSPARENCY)
+                        .setDepthTestState(LEQUAL_DEPTH_TEST)
+                        .setCullState(CULL)
+                        .setLightmapState(LIGHTMAP)
+                        .setOverlayState(NO_OVERLAY)
+                        .setLayeringState(NO_LAYERING)
+                        .setOutputState(MAIN_TARGET)
+                        .setTexturingState(DEFAULT_TEXTURING)
+                        .setWriteMaskState(COLOR_DEPTH_WRITE)
+                        .setLineState(DEFAULT_LINE)
+                        .createCompositeState(true)
+        );
+        var baseSetup = this.setupState;
+        this.setupState = () -> {
+            baseSetup.run();
+            RenderSystem.setShaderTexture(0, texture);
+        };
+    }
 
-	private static final ImmutableList<RenderStateShard> GENERAL_STATES = RenderUtils.getRenderStateShards();
+    public SlideRenderType(ResourceLocation texture) {
+        super(Slideshow.ID + "_icon", DefaultVertexFormat.BLOCK,
+                VertexFormat.Mode.QUADS, 256, false, true,
+                CompositeState.builder()
+                        .setShaderState(RENDERTYPE_TEXT_SEE_THROUGH_SHADER)
+                        .setTransparencyState(TRANSLUCENT_TRANSPARENCY)
+                        .setDepthTestState(LEQUAL_DEPTH_TEST)
+                        .setCullState(CULL)
+                        .setLightmapState(LIGHTMAP)
+                        .setOverlayState(NO_OVERLAY)
+                        .setLayeringState(NO_LAYERING)
+                        .setOutputState(MAIN_TARGET)
+                        .setTexturingState(DEFAULT_TEXTURING)
+                        .setWriteMaskState(COLOR_DEPTH_WRITE)
+                        .setLineState(DEFAULT_LINE)
+                        .createCompositeState(true));
+        var baseSetup = this.setupState;
+        this.setupState = () -> {
+            baseSetup.run();
+            RenderSystem.setShaderTexture(0, texture);
+        };
+    }
 
-	private final int mHashCode;
-
-	public SlideRenderType(int texture) {
-		super(Slideshow.ID, DefaultVertexFormat.BLOCK,
-				RenderUtils.getMode(), 256, false, true,
-				() -> {
-					GENERAL_STATES.forEach(RenderStateShard::setupRenderState);
-					RenderUtils.startDrawingTexture(texture);
-				},
-				() -> GENERAL_STATES.forEach(RenderStateShard::clearRenderState));
-		mHashCode = Objects.hash(super.hashCode(), GENERAL_STATES, texture);
-	}
-
-	SlideRenderType(ResourceLocation texture) {
-		super(Slideshow.ID, DefaultVertexFormat.BLOCK,
-				RenderUtils.getMode(), 256, false, true,
-				() -> {
-					GENERAL_STATES.forEach(RenderStateShard::setupRenderState);
-					UtilitiesClient.beginDrawingTexture(texture);
-				},
-				() -> GENERAL_STATES.forEach(RenderStateShard::clearRenderState));
-		mHashCode = Objects.hash(super.hashCode(), GENERAL_STATES, texture);
-	}
-
-	@Override
-	public int hashCode() {
-		return mHashCode;
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		return this == o;
-	}
+    @Override
+    public boolean equals(Object o) {
+        return this == o;
+    }
 }
